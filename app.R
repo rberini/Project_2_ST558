2b0f0b0a3f293f681e5eb0a66971360f5ccf2b0f
rberini <rmberin2@ncsu.edu>
2024-10-21 06:51:19 -0400

    file setup

---
+++ app.R
@@ -0,0 +1,12 @@
+library(shiny)
+library(bslib)
+library(tidyverse)
+
+#ui section
+ui <- fluidPage()
+
+#server section
+server <- function(input, output, session) {}
+
+#run section
+shinyApp(ui, server)
